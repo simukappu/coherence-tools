88488e5f4b521c62a5d90d16aa26583c55bdf70e
simukappu <shota.yamazaki.8@gmail.com>
2016-04-21 20:30:36 +0900

    Add javadoc to PortableEntry

--- write-behind-tools/src/main/java/com/simukappu/coherence/writequeue/PortableEntry.java
+++ write-behind-tools/src/main/java/com/simukappu/coherence/writequeue/PortableEntry.java
@@ -8,6 +8,16 @@ import com.tangosol.io.pof.PofReader;
 import com.tangosol.io.pof.PofWriter;
 import com.tangosol.io.pof.PortableObject;
 
+/**
+ * Custom java.util.Map.Entry implementation class supporting POF serializer
+ * 
+ * @author Shota Yamazaki
+ *
+ * @param <K>
+ *            Key class
+ * @param <V>
+ *            Value class
+ */
 public class PortableEntry<K, V> implements Entry<K, V>, Serializable, PortableObject {
 
 	/**
@@ -18,38 +28,77 @@ public class PortableEntry<K, V> implements Entry<K, V>, Serializable, PortableO
 	private K key;
 	private V value;
 
+	/**
+	 * Default constructor
+	 */
 	public PortableEntry() {
 		super();
 	}
 
+	/**
+	 * Constructor using fields
+	 * 
+	 * @param key
+	 *            Key of the entry
+	 * @param value
+	 *            Value of the entry
+	 */
 	public PortableEntry(K key, V value) {
 		super();
 		this.key = key;
 		this.value = value;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map.Entry#getKey()
+	 */
 	@Override
 	public K getKey() {
 		return key;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map.Entry#getValue()
+	 */
 	@Override
 	public V getValue() {
 		return value;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map.Entry#setValue(java.lang.Object)
+	 */
 	@Override
 	public V setValue(V value) {
 		this.value = value;
 		return value;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.tangosol.io.pof.PortableObject#readExternal(com.tangosol.io.pof.
+	 * PofReader)
+	 */
 	@Override
 	public void readExternal(PofReader reader) throws IOException {
 		key = reader.readObject(0);
 		value = reader.readObject(1);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.tangosol.io.pof.PortableObject#writeExternal(com.tangosol.io.pof.
+	 * PofWriter)
+	 */
 	@Override
 	public void writeExternal(PofWriter writer) throws IOException {
 		writer.writeObject(0, key);
